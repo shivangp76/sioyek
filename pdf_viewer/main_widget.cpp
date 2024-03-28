4545afb21131a5f410b784f4daf18e2020b7d1de
ahrm <a.hr.mostafavi@gmail.com>
2024-03-28 13:20:23 +0330

    Fix a bug where scrollbar was not working

--- pdf_viewer/main_widget.cpp
+++ pdf_viewer/main_widget.cpp
@@ -851,7 +851,7 @@ MainWidget::MainWidget(fz_context* mupdf_context,
 #endif
 
     central_widget = new QWidget(this);
-    central_widget->setAttribute(Qt::WA_TransparentForMouseEvents);
+    // central_widget->setAttribute(Qt::WA_TransparentForMouseEvents);
 
     inverse_search_command = INVERSE_SEARCH_COMMAND;
     pdf_renderer = new PdfRenderer(4, should_quit_ptr, mupdf_context);
