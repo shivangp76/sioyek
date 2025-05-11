9ba3291a8fd79243de6c2ca580aded25c8314b02
ahrm <a.hr.mostafavi@gmail.com>
2025-05-11 18:58:23 +0330

    Changes

--- qsb_shaders/rebuild.sh
+++ qsb_shaders/rebuild.sh
@@ -1,2 +1,7 @@
 # rebuild the *.frag and *.vert shaders using qsb into the prebuilt folder
 ls *.frag *.vert | xargs -I {} qsb --qt6 -o prebuilt/{}.qsb {}
+
+# equivalent windows batch script:
+# @echo off
+# for %%f in (*.frag *.vert) do qsb --qt6 -o prebuilt/%%f.qsb %%f
+
